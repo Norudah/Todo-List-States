ec09b7ce888e4a8357d3da964217f5f01ff0a9f4
Norudah <pierucci.romain@gmail.com>
2020-12-09 14:29:28 +0100

    Scaffold base structure achieved

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,8 +5,59 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
-        child: Text("hello"),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              top: 60,
+              left: 30,
+              right: 30,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 45,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  radius: 30,
+                  backgroundColor: Colors.white,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Todo app'",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(height: 2),
+                Text(
+                  "13 Tasks",
+                  style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 15),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  )),
+            ),
+          ),
+        ],
       ),
     );
   }
